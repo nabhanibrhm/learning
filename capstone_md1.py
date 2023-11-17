b2b337a2468a694f52c96f00948588f7ed9757c2
ratubalkiskh <ratubalkiskhh@gmail.com>
2023-11-17 21:24:32 +0700

    new py for capstone md1

---
+++ capstone_md1.py
@@ -0,0 +1,78 @@
+#1. membuat fungsi untuk menu utama
+
+import sys
+
+data = {
+    "id_karyawan": None,
+    "nik": None,
+    "nama_karyawan": None,
+    "alamat_karyawan": None,
+    "divisi": None,
+    "contact": None
+}
+
+def create():
+    global data
+    print('Masukkan Data Karyawan')
+
+
+    for key in data:
+        data[key] = input(f'Masukkan {key.replace("_", " ").title()}: ')
+    
+    # id_karyawan = int(input('masukkan id karyawan: '))
+    # nik = int(input('masukkan NIK karyawan: '))
+    # nama_karyawan = input('masukkan nama karyawan: ')
+    # alamat_karyawan = input('masukkan alamat karyawan: ')
+    # divisi = input('divisi karyawan: ')
+    # contact = int(input('masukkan no.tlp karyawan: '))
+    menu()
+
+
+def read():
+    print('fungsi cari data')
+    print(data)
+
+def update():
+    print('fungsi update data')
+
+def delete():
+    print('fungsi hapus data')
+
+def pilihan():
+    pilihan = input('Masukkan Pilihan :')
+
+def exit():
+    sys.exit()
+
+def menu():
+    print('Menu Utama\n')
+    print('1. Input Data')
+    print('2. Lihat Data')
+    print('3. Ubah Data')
+    print('4. Hapus Data')
+    print('5. Keluar\n\n')
+
+
+    while True:
+        try:
+            pilihan = int(input('Masukkan Pilihan: '))
+            break
+        except:
+            print('Pilihan salah. Harap masukkan angka yang valid.')
+
+    if pilihan == 1:
+        create()
+    elif pilihan == 2:
+        read()
+    elif pilihan == 3:
+        update()
+    elif pilihan == 4:
+        delete()
+    elif pilihan == 5:
+        exit()
+    
+
+
+
+if __name__ == "__main__":
+    menu()
